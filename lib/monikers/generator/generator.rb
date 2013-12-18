8282b04eee7e52de6f1d6211dbf909cf1d8300f6
Ben Di <bdifrancesco@ascensionpress.com>
2013-12-18 14:20:17 -0500

    Code to generate the pre-cached data structure

---
+++ lib/monikers/generator/generator.rb
@@ -0,0 +1,40 @@
+module Monikers
+  module Generator
+    CSV_FILE_PATH = '../../data/monikers_list.csv'.freeze
+    OUT_FILE_PATH = '../../data/monikers_hash.rb'.freeze
+
+    def self.generate_cache
+      monikers = Hash.new
+
+      File.open(Monikers::Generator::CSV_FILE_PATH, 'r') do |file_handle|
+        file_handle.each do |line|
+          split_line = line.split(/,/)
+
+          name = split_line[1].strip.downcase
+          moniker = split_line[2].strip.downcase
+
+          if monikers.key?(name)
+            if !monikers[name].include?(moniker)
+              monikers[name]<< moniker
+            end
+          else
+            monikers[name] = Array.new<< moniker
+          end
+
+          if monikers.key?(moniker)
+            if !monikers[moniker].include?(name)
+              monikers[moniker]<< name
+            end
+          else
+            monikers[moniker] = Array.new<< name
+          end
+        end
+      end
+
+      File.open(OUT_FILE_PATH, 'w') { |out_file| PP.pp(monikers, out_file) }
+
+      return monikers
+    end
+
+  end
+end
--- monikers.gemspec
+++ monikers.gemspec
@@ -16,4 +16,7 @@ Gem::Specification.new do |gem|
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.require_paths = ["lib"]
+
+   s.add_dependency 'pp'
+   s.add_development_dependency 'rspec', '~> 2.10'
 end
